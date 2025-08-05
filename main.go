c2e0029d1f93564a3cc95791f3aca1fb9d13eede
kishism <morrg25@gmail.com>
2025-08-05 23:17:46 +0630

    Added italic, underline and update the HTMX fragment swap

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 const PORT = ":8000"
@@ -15,6 +16,10 @@ func main() {
 
 	http.HandleFunc("/", editorHandler)       // GET - Show the editor
 	http.HandleFunc("/save", saveHandler)     // POST - Save content
+	http.HandleFunc("/view", viewHandler)
+	http.HandleFunc("/format/bold", boldHandler)
+	http.HandleFunc("/format/italic", italicHandler)
+	http.HandleFunc("/format/underline", underlineHandler)
 
 	log.Fatal(http.ListenAndServe(PORT, nil))
 }
@@ -56,4 +61,121 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, `<div id="save-status" style="color: green;">Saved</div>`)
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	savedContent := ""
+
+	if data, err := os.ReadFile("saved.html"); err == nil {
+		savedContent = string(data) 
+	}
+
+	tmpl := template.Must(template.ParseFiles("templates/view.tmpl.html"))
+
+	viewData := struct {
+		ViewContent template.HTML
+	}{
+		ViewContent: template.HTML(savedContent),
+	}
+
+	if err := tmpl.Execute(w, viewData); err != nil {
+		http.Error(w, "Template execution error", http.StatusInternalServerError)
+		log.Println("View template error:", err)
+	}
+}
+
+func boldHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	content := r.FormValue("content-hidden")
+	startStr := r.FormValue("start")
+	endStr := r.FormValue("end")
+
+	start, _ := strconv.Atoi(startStr)
+	end, _ := strconv.Atoi(endStr)
+
+	if start >= end || start < 0 || end > len(content) {
+		http.Error(w, "Invalid selection", http.StatusBadRequest)
+		return
+	}
+
+	newContent := content[:start] + "<b>" + content[start:end] + "</b>" + content[end:]
+
+	tmpl := template.Must(template.ParseFiles("templates/partial_editable.tmpl.html"))
+    err = tmpl.Execute(w, struct {
+        InitialContent string
+    }{InitialContent: newContent})
+
+    if err != nil {
+        http.Error(w, "Server Error", http.StatusInternalServerError)
+        return
+    }
+}
+
+func italicHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	content := r.FormValue("content-hidden")
+	startStr := r.FormValue("start")
+	endStr := r.FormValue("end")
+
+	start, _ := strconv.Atoi(startStr)
+	end, _ := strconv.Atoi(endStr)
+
+	if start >= end || start < 0 || end > len(content) {
+		http.Error(w, "Invalid selection", http.StatusBadRequest)
+		return
+	}
+
+	newContent := content[:start] + "<i>" + content[start:end] + "</i>" + content[end:]
+
+	tmpl := template.Must(template.ParseFiles("templates/partial_editable.tmpl.html"))
+    err = tmpl.Execute(w, struct {
+        InitialContent string
+    }{InitialContent: newContent})
+
+    if err != nil {
+        http.Error(w, "Server Error", http.StatusInternalServerError)
+        return
+    }
+}
+
+func underlineHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	content := r.FormValue("content-hidden")
+	startStr := r.FormValue("start")
+	endStr := r.FormValue("end")
+
+	start, _ := strconv.Atoi(startStr)
+	end, _ := strconv.Atoi(endStr)
+
+	if start >= end || start < 0 || end > len(content) {
+		http.Error(w, "Invalid selection", http.StatusBadRequest)
+		return
+	}
+
+	newContent := content[:start] + "<u>" + content[start:end] + "</u>" + content[end:]
+
+	tmpl := template.Must(template.ParseFiles("templates/partial_editable.tmpl.html"))
+    err = tmpl.Execute(w, struct {
+        InitialContent string
+    }{InitialContent: newContent})
+
+    if err != nil {
+        http.Error(w, "Server Error", http.StatusInternalServerError)
+        return
+    }
 }
